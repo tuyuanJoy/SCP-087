ad845a96a45c13eae1a4b16246d62eecc4756c1e
unknown <TUYUAN0504@gmail.com>
2022-11-17 01:43:37 +0900

    Test Field

---
+++ ScriptTestingField/Assets/Scripts/PlayerTeleport.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTeleport : MonoBehaviour
+{
+    public Transform teleportTarget;
+    public GameObject playerBody;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log("x: "+ playerBody.transform.position.x + " y: " + playerBody.transform.position.y);
+    }
+
+    private void OnTriggerEnter(Collider Other)
+    {
+        Debug.Log("Collided");
+        playerBody.transform.position = teleportTarget.transform.position;
+    }
+}
